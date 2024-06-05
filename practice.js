56a440fabbd0ce56e610bef5c8a684de1943e0ac
practice.1 <abdullahansar201@gmail.com>
2024-06-05 05:00:08 -0700

    deploying

---
+++ practice.js
@@ -0,0 +1,61 @@
+//Q 1
+// How to break JavaScript Code into several lines? 
+
+
+//Answer 1
+
+
+
+//Q 2  Write the code for adding new elements dynamically?
+
+
+//Answer 2
+// function addNewElement() {
+//     var newElement = document.createElement("p");
+//     newElement.textContent = "new element!";
+//     var container = document.getElementById("container");
+//     container.appendChild(newElement);
+// }
+
+
+//Q 4 
+// How can the style/class of an element be changed?
+
+
+//Answer 4
+// function changeStyle() {
+//     var element = document.getElementById("myElement");
+//     element.style.color = "green"; 
+// }
+
+// // Function to change the class
+// function changeClass() {
+//     var element = document.getElementById("myElement");
+//     element.className = "new-style"; 
+// }
+
+
+
+//Q 5
+// How can you convert the string of any base to an integer in JavaScript?
+
+
+//Answer 5
+// var convertString = "1A"; 
+// var decimalNumber = parseInt(convertString, 16);
+
+// console.log(decimalNumber); 
+
+
+//Q 6
+// What is the function of the delete operator?
+
+
+//Answer 6
+// const myObject = { name: 'Abdullah', age: 17, city: 'karachi' };
+// delete myObject.age;
+// console.log(myObject); 
+
+
+
+
